bcbd8e8cb7622e458932017bc3d53950e1390e5a
root <root@DESKTOP-E4GFIVI.localdomain>
2025-09-12 22:50:30 +0200

    Barre d'outils synchronisée

--- src/main_windows.py
+++ src/main_windows.py
@@ -1,4 +1,5 @@
-from PyQt6.QtGui import QAction
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtWidgets import QLabel, QMainWindow
 
 
@@ -11,6 +12,7 @@ class MainWindow(QMainWindow):
         label = QLabel("Hello, World!")
         self.setCentralWidget(label)
         self.setup_menu_bar()
+        self.setup_toolbar()
 
         # Barre de statut
         if (status_bar := self.statusBar()) is not None:
@@ -22,40 +24,72 @@ class MainWindow(QMainWindow):
 
         action_nouveau = QAction("&Nouveau", self)
         action_nouveau.setShortcut("Ctrl+N")
-        action_nouveau.triggered.connect(self.nouveau)
+        action_nouveau.triggered.connect(self.action_nouveau)
         menu_bar.addAction(action_nouveau)
 
         action_ouvrir = QAction("&Ouvrir", self)
         action_ouvrir.setShortcut("Ctrl+O")
-        action_ouvrir.triggered.connect(self.ouvrir)
+        action_ouvrir.triggered.connect(self.action_ouvrir)
         menu_bar.addAction(action_ouvrir)
 
         action_sauvegarder = QAction("&Sauvegarder", self)
         action_sauvegarder.setShortcut("Ctrl+S")
-        action_sauvegarder.triggered.connect(self.sauvegarder)
+        action_sauvegarder.triggered.connect(self.action_sauvegarder)
         menu_bar.addAction(action_sauvegarder)
 
         action_quitter = QAction("&Quitter", self)
         action_quitter.setShortcut("Ctrl+Q")
-        action_quitter.triggered.connect(self.quitter)
+        action_quitter.triggered.connect(self.action_quitter)
         menu_bar.addAction(action_quitter)
 
-    def nouveau(self) -> None:
+    def action_nouveau(self) -> None:
         # Barre de statut
         if (status_bar := self.statusBar()) is not None:
             status_bar.showMessage("Nouvelle fenêtre ouverte", 1000)
 
-    def ouvrir(self) -> None:
+    def action_ouvrir(self) -> None:
         # Barre de statut
         if (status_bar := self.statusBar()) is not None:
             status_bar.showMessage("Fenêtre ouverte", 1000)
 
-    def sauvegarder(self) -> None:
+    def action_sauvegarder(self) -> None:
         # Barre de statut
         if (status_bar := self.statusBar()) is not None:
             status_bar.showMessage("Fenêtre sauvegardée", 1000)
 
-    def quitter(self) -> None:
+    def action_quitter(self) -> None:
         # Barre de statut
         if (status_bar := self.statusBar()) is not None:
             status_bar.showMessage("Application quittée", 1000)
+
+    def setup_toolbar(self) -> None:
+        if (toolbar := self.addToolBar("Principal")) is None:
+            return
+
+        toolbar.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
+
+        action_nouveau = QAction("&Nouveau", self)
+        action_nouveau.setIcon(QIcon("icons/new.png"))
+        action_nouveau.triggered.connect(self.action_nouveau)
+        toolbar.addAction(action_nouveau)
+
+        toolbar.addSeparator()
+
+        action_ouvrir = QAction("&Ouvrir", self)
+        action_ouvrir.setIcon(QIcon("icons/open.png"))
+        action_ouvrir.triggered.connect(self.action_ouvrir)
+        toolbar.addAction(action_ouvrir)
+
+        toolbar.addSeparator()
+
+        action_sauvegarder = QAction("&Sauvegarder", self)
+        action_sauvegarder.setIcon(QIcon("icons/save.png"))
+        action_sauvegarder.triggered.connect(self.action_sauvegarder)
+        toolbar.addAction(action_sauvegarder)
+
+        toolbar.addSeparator()
+
+        action_quitter = QAction("&Quitter", self)
+        action_quitter.setIcon(QIcon("icons/open.png"))
+        action_quitter.triggered.connect(self.action_quitter)
+        toolbar.addAction(action_quitter)
